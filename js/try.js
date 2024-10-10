e829dfaa83725ece916648f834e2baead4ea8aec
Moinuddin-dotcom <moinchy7@gmail.com>
2024-10-10 17:19:59 +0600

    try added

---
+++ js/try.js
@@ -0,0 +1,29 @@
+const message = [
+    "Initializing Hacking...",
+    "Reading your files...",
+    "Password file detected...",
+    "Sending all password files and data to server...",
+    "Cleaning up...",
+    "You are hacked by Moin Uddin..."
+]
+
+const visite = () => {
+    document.getElementById('change').style.backgroundColor = 'green'
+
+    let modal = document.getElementById('clickVisite')
+
+    modal.showModal()
+    index = 0
+    const interval = setInterval(() => {
+
+        let innerItem = document.getElementById('innerModal')
+        innerItem.innerHTML = `<p>${message[index]}</p>`
+        index++
+
+        if (index > message.length) {
+            clearInterval(interval)
+            document.getElementById('clickVisite').close()
+            // modal.close()
+        }
+    }, 1000);
+}
